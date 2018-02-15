b4b190e009071aabc7abe3b57d1a2af04a637a2a
nisdis <niszim@gmail.com>
2018-02-15 13:53:26 +0530

    UI changes first commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -39,65 +39,70 @@ class _ChewieDemoState extends State<ChewieDemo> {
         appBar: new AppBar(
           title: new Text(widget.title),
         ),
+        bottomNavigationBar: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+                      children: <Widget>[
+
+                          new FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                _platform = TargetPlatform.android;
+                              });
+                            },
+                            highlightColor: Colors.greenAccent,
+                            color: Colors.green,
+                            child: new Padding(
+                              child: new Text("Android controls"),
+                              padding: new EdgeInsets.symmetric(vertical: 16.0),
+                            ),
+                          ),
+
+                         new FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                _platform = TargetPlatform.iOS;
+                              });
+                            },
+                            highlightColor: Colors.blueAccent,
+                            color: Colors.blue,
+                            child: new Padding(
+                              padding: new EdgeInsets.symmetric(vertical: 16.0),
+                              child: new Text("iOS controls"),
+                            ),
+                          ),
+
+                      ],
+                    ),
         body: new Column(
           children: <Widget>[
-            new Expanded(
+            new Container(
               child: new Center(
                 child: new Chewie(
                   widget.controller,
                   aspectRatio: 3 / 2,
-                  autoPlay: true,
+                  autoPlay: false,
                   looping: true,
 
                   // Try playing around with some of these other options:
 
-                  // showControls: false,
-                  // materialProgressColors: new ChewieProgressColors(
-                  //   playedColor: Colors.red,
-                  //   handleColor: Colors.blue,
-                  //   backgroundColor: Colors.grey,
-                  //   bufferedColor: Colors.lightGreen,
-                  // ),
-                  // placeholder: new Container(
-                  //   color: Colors.grey,
-                  // ),
-                  // autoInitialize: true,
+                  showControls: true,
+                  materialProgressColors: new ChewieProgressColors(
+                    playedColor: Colors.red,
+                    handleColor: Colors.blue,
+                    backgroundColor: Colors.grey,
+                    bufferedColor: Colors.lightGreen,
+                  ),
+                  placeholder: new Container(
+                    color: Colors.grey,
+                  ),
+                  autoInitialize: true,
                 ),
               ),
             ),
-            new Row(
-              children: <Widget>[
-                new Expanded(
-                  child: new FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _platform = TargetPlatform.android;
-                      });
-                    },
-                    child: new Padding(
-                      child: new Text("Android controls"),
-                      padding: new EdgeInsets.symmetric(vertical: 16.0),
-                    ),
-                  ),
-                ),
-                new Expanded(
-                  child: new FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _platform = TargetPlatform.iOS;
-                      });
-                    },
-                    child: new Padding(
-                      padding: new EdgeInsets.symmetric(vertical: 16.0),
-                      child: new Text("iOS controls"),
-                    ),
-                  ),
-                )
-              ],
-            )
           ],
         ),
       ),
     );
   }
 }
+
